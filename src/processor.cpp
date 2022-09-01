4b694085e55c1641e3b7964340f9fefd83933f55
JFaustoGC <186P0640@itspozarica.edu.mx>
2022-08-31 22:05:35 -0500

    Revert "first commin"
    This reverts commit 38760005a7538d7c336c75b9b0636f92476e13e7.

--- src/processor.cpp
+++
@@ -1,9 +0,0 @@
-#include "processor.h"
-#include "linux_parser.h"    
-
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { 
-    long total = LinuxParser::Jiffies();
-    long active = LinuxParser::ActiveJiffies();
-    return active * (1.f / total);
- }
